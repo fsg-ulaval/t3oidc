8db3cd1663409e74dd09e77ca6a93c3d0a2a4f69
Cyril Janody <cyril.janody@fsg.ulaval.ca>
2021-02-19 14:13:35 -0500

    [TASK] Add and register the new authentication service class

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,7 +12,11 @@ defined('TYPO3_MODE') || die();
         FSG\Oidc\Domain\Model\Dto\ExtensionConfiguration::class
     );
 
+    $subTypes = [];
     if ((bool)$settings->isEnableBackendAuthentication()) {
+        $subTypes[] = 'getUserBE';
+        $subTypes[] = 'authUserBE';
+
         $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1613676682] = [
             'provider'   => \FSG\Oidc\LoginProvider\OpenIDConnectSignInProvider::class,
             'sorting'    => 75,
@@ -20,6 +24,25 @@ defined('TYPO3_MODE') || die();
             'label'      => 'LLL:EXT:t3oidc/Resources/Private/Language/locallang.xlf:backend.login.switch.label',
         ];
     }
+
+    $subTypes = implode(',', $subTypes);
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+        $extKey,
+        'auth',
+        \FSG\Oidc\Service\AuthenticationService::class,
+        [
+            'title' => 'LLL:EXT:t3oidc/Resources/Private/Language/locallang.xlf:authentication.service',
+            'description' => 'Authentication service for OpenID Connect.',
+            'subtype' => $subTypes,
+            'available' => true,
+            'priority' => 75,
+            'quality' => 75,
+            'os' => '',
+            'exec' => '',
+            'className' => \FSG\Oidc\Service\AuthenticationService::class,
+        ]
+    );
 
     // Require 3rd-party libraries, in case TYPO3 does not run in composer mode
     $pharFileName = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($extKey)
